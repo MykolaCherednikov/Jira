bd5cb6415c5aa0f7d19f502f46f8b8498fd54fd3
unknown <mykola.cherednikov@nure.ua>
2023-05-10 12:00:44 +0300

    CK22-74 Додано реєстрацію на веб-сервер

---
+++ Web-Server/ChatServer/Controllers/UsersController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ChatServer.Data;
+using ChatServer.Models;
+using ChatServer.DTO;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ChatServer.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly ChatServerContext _context;
+
+        public UsersController(ChatServerContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetByID")]
+        public async Task<ActionResult<User>> GetUserByID(int id_user)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x => x.id_user == id_user);
+            if (user == null)
+            {
+                string message = "Not Found";
+                return StatusCode(400, message);
+            }
+
+            return user;
+        }
+
+        [HttpGet("GetByEmail")]
+        public async Task<ActionResult<User>> GetUserByEmail(string email)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x => x.email == email);
+            if (user == null)
+            {
+                string message = "Not Found";
+                return StatusCode(400, message);
+            }
+            return user;
+        }
+
+        /// <summary>
+        /// Get data from login.
+        /// </summary>
+        [HttpPost("Login")]
+        public async Task<ActionResult<User>> Login(UserLoginDTO user_request)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x => x.email == user_request.email);
+            if(user == null)
+            {
+                string message = "Incorrect email";
+                return StatusCode(400, message);
+            }
+            if(user.password != user_request.password)
+            {
+                string message = "Incorrect password";
+                return StatusCode(400, message);
+            }
+            return user;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<User>> Register(UserRegisterDTO user_request)
+        {
+            var user = await _context.User.FirstOrDefaultAsync(x => x.email == user_request.email);
+            if (user != null)
+            {
+                string message = "User with this login alredy exsists";
+                return StatusCode(400, message);
+            }
+
+            var tempuser = new User
+            {
+                nickname = user_request.nickname,
+                email = user_request.email,
+                password = user_request.password
+            };
+          
+            _context.User.Add(tempuser);
+            await _context.SaveChangesAsync();
+            return await Login(new UserLoginDTO() { email = tempuser.email, password = tempuser.password});
+        }
+
+        [HttpGet("GetUsersByChatId")]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsersByChatId(int id_chat)
+        {
+            var users = await _context.UserToChat.Where(x => x.rk_id_chat == id_chat).Select(x => x.RkIdUserNavigation).ToListAsync();
+
+            if (users.Count == 0)
+            {
+                string message = "Not Found";
+                return StatusCode(400, message);
+            }
+
+            return users;
+        }
+    }
+}
---
+++ Web-Server/ChatServer/DTO/UserRegisterDTO.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ChatServer.DTO
+{
+    public class UserRegisterDTO
+    {
+        [Required]
+        public string nickname { get; set; } = null!;
+        [Required]
+        public string email { get; set; } = null!;
+        [Required]
+        public string password { get; set; } = null!;
+    }
+}
